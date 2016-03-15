56572113ace2f263eba15203a9ff5e922cfc0b11
Jan Bracker <jan.bracker@googlemail.com>
2016-03-15 17:00:12 +0000

    Try to express the bind instances in a more concrete way without using Undecidable instances for on of the examples

--- examples/effect/MainSupermonad3.hs
+++ examples/effect/MainSupermonad3.hs
@@ -10,7 +10,7 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 
-{-# LANGUAGE UndecidableInstances #-}
+--{-# LANGUAGE UndecidableInstances #-}
 
 -- Ignore our orphan instance in this file.
 {-# OPTIONS_GHC -fno-warn-orphans #-}
@@ -24,28 +24,18 @@ import qualified Control.Effect as E
 import Control.Effect ( Effect, Plus, Unit, Inv )
 import Control.Effect.CounterNat
 
-instance ( Effect m
-         , Inv m s (Unit m)
-         , s ~ Plus m s (Unit m)
-         ) => Functor (m (s :: k)) where
+import GHC.TypeLits
+
+instance Functor (Counter (s :: Nat)) where
   fmap f ma = ma E.>>= (E.return . f)
 
-instance ( Effect m
-         , h ~ Plus m f g
-         , Inv m f g
-         , f ~ Plus m f (Unit m)
-         , g ~ Plus m g (Unit m)
-         , h ~ Plus m h (Unit m)
-         , Inv m f (Unit m)
-         , Inv m g (Unit m)
-         , Inv m h (Unit m)
-         ) => Bind (m (f :: k)) (m (g :: k)) (m (h :: k)) where
+instance (h ~ (f + g)) => Bind (Counter (f :: Nat)) (Counter (g :: Nat)) (Counter (h :: Nat)) where
   (>>=) = (E.>>=)
 
-instance (Effect m, h ~ Unit m) => Return (m (h :: k)) where
+instance (h ~ 0) => Return (Counter (h :: Nat)) where
   return = E.return
 
-instance Fail (m (h :: k)) where
+instance Fail (Counter (h :: Nat)) where
   fail = E.fail
 
 main :: IO ()
