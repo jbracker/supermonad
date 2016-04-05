75369736e8000526f92ed9518d2a4bffc35c04ce
Jan Bracker <jan.bracker@googlemail.com>
2016-04-05 16:58:22 +0100

    Unfinished work on solving algorithm

--- src/Control/Supermonad/Plugin/Solving.hs
+++ src/Control/Supermonad/Plugin/Solving.hs
@@ -1,5 +1,39 @@
  
 module Control.Supermonad.Plugin.Solving 
-  ( 
+  ( solveConstraintGroup
   ) where
 
+import Control.Monad ( forM )
+
+import TcRnTypes ( Ct )
+import TyCon ( TyCon )
+import Type ( TyVar )
+import TcPluginM ( TcPluginM )
+
+import Control.Supermonad.Plugin.Environment ( SupermonadPluginM, printMsg )
+import Control.Supermonad.Plugin.Constraint ( WantedCt )
+import Control.Supermonad.Plugin.Separation ( componentTopTyCons, componentTopTcVars )
+import Control.Supermonad.Plugin.Utils ( associations )
+
+solveConstraintGroup :: [WantedCt] -> SupermonadPluginM [Ct]
+solveConstraintGroup ctGroup = do
+  printMsg $ "Solving Group..."
+  printMsg $ "Size = " ++ show (length ctGroup) ++ "; "
+          ++ "Vars = " ++ show (length tyConVars) ++ "; "
+          ++ "Base Size = " ++ show (length tyConBase) ++ "; "
+          ++ "Associations = " ++ show (length assocs) ++ ";"
+  
+  validAssocs <- forM assocs $ \assoc -> do
+    undefined
+  
+  return []
+  
+  where
+    tyConVars :: [TyVar]
+    tyConVars = componentTopTcVars ctGroup
+    
+    tyConBase :: [TyCon]
+    tyConBase = componentTopTyCons ctGroup
+    
+    assocs :: [[(TyVar, TyCon)]]
+    assocs = associations $ fmap (\tv -> (tv, tyConBase)) tyConVars
