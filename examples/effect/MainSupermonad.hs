2ce38704ac667821ef72b49ea1a8c6822ebe7346
Jan Bracker <jan.bracker@googlemail.com>
2016-04-18 18:08:00 +0100

    Make supermonad instances in the first effect exmaple concrete, they send the type checker into a loop

--- examples/effect/MainSupermonad.hs
+++ examples/effect/MainSupermonad.hs
@@ -20,32 +20,30 @@
 
 import Control.Supermonad.Prelude
 
-import Control.Effect ( Effect, Plus, Unit, Inv )
+import Control.Effect ( Plus, Inv )
 import qualified Control.Effect as E
 import Control.Effect.State
 
-instance ( Effect m
-         , Inv m s (Unit m)
-         , s ~ Plus m s (Unit m)
-         ) => Functor (m (s :: k)) where
+instance ( Inv State s '[]
+         , s ~ Plus State s '[]
+         ) => Functor (State (s :: [*])) where
   fmap f ma = ma E.>>= (E.return . f)
 
-instance ( Effect m
-         , h ~ Plus m f g
-         , Inv m f g
-         , f ~ Plus m f (Unit m)
-         , g ~ Plus m g (Unit m)
-         , h ~ Plus m h (Unit m)
-         , Inv m f (Unit m)
-         , Inv m g (Unit m)
-         , Inv m h (Unit m)
-         ) => Bind (m (f :: k)) (m (g :: k)) (m (h :: k)) where
+instance ( h ~ Plus State f g
+         , Inv State f g
+         , f ~ Plus State f '[]
+         , g ~ Plus State g '[]
+         , h ~ Plus State h '[]
+         , Inv State f '[]
+         , Inv State g '[]
+         , Inv State h '[]
+         ) => Bind (State (f :: [*])) (State (g :: [*])) (State (h :: [*])) where
   (>>=) = (E.>>=)
 
-instance (Effect m, h ~ Unit m) => Return (m (h :: k)) where
+instance (h ~ '[]) => Return (State (h :: [*])) where
   return = E.return
 
-instance Fail (m (h :: k)) where
+instance Fail (State (h :: [*])) where
   fail = E.fail
 
 main :: IO ()
@@ -54,14 +52,16 @@ main = do
     ( write "abc" )
     ( Ext (Var :-> 0 :! Eff) (Ext (Var :-> [] :! Eff) Empty) )
 
-varC = Var :: Var "count"
-varS = Var :: Var "out"
+varC :: Var "count"
+varC = Var 
+varS :: Var "out"
+varS = Var
 
-incC :: State '["count" :-> Int :! RW] Int
+incC :: State '["count" :-> Int :! 'RW] Int
 incC = do { x <- get varC; put varC (x + 1); return (x + 1) }
 
-writeS :: [a] -> State '["out" :-> [a] :! RW] ()
+writeS :: [a] -> State '["out" :-> [a] :! 'RW] ()
 writeS y = do { x <- get varS; put varS (x ++ y) }
 
-write :: [a] -> State '["count" :-> Int :! RW, "out" :-> [a] :! RW] ()
+write :: [a] -> State '["count" :-> Int :! 'RW, "out" :-> [a] :! 'RW] ()
 write x = do { writeS x; _ <- incC; return () }
