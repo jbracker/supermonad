c539af1a48aa1e63221fab88c06b2b10cef63d46
Jan Bracker <jan.bracker@googlemail.com>
2016-10-24 09:57:51 +0100

    Generalize the instance dictionary type.

--- src/Control/Supermonad/Plugin/Dict.hs
+++ src/Control/Supermonad/Plugin/Dict.hs
@@ -1,69 +1,45 @@
 
 -- | Provides the type for supermonad dictionaries.
 module Control.Supermonad.Plugin.Dict 
-  ( SupermonadDict
-  , BindInst, ApplicativeInst, ReturnInst
-  , emptyDict, insertDict, lookupDict
-  , lookupDictBind, lookupDictApplicative, lookupDictReturn
-  , allTyConsDict ) where
+  ( InstanceDict
+  , emptyInstDict, insertInstDict, lookupInstDict
+  , allTyConsInstDict ) where
 
 import qualified Data.Set as S
 import qualified Data.Map.Strict as M
 
-import Control.Monad ( join )
-
-import InstEnv ( ClsInst(..) )
+import Class ( Class )
+import InstEnv ( ClsInst )
 import TyCon ( TyCon )
 
--- | A dictionary associating supermonad type constructors with their
---   'Control.Supermonad.Bind', 'Control.Supermonad.Applicative' 
---   and 'Control.Supermonad.Return' instances.
+-- | A dictionary associating supermonad type constructors and classes with 
+--   their instances.
 --   This is essentially a lookup table for instances associated with a 
---   given type constructor.
---   Note that the bind instance is optional because a given 
---   type constructor may only be a superapplicative, but not a supermonad.
-newtype SupermonadDict = SDict (M.Map TyCon (Maybe BindInst, ApplicativeInst, ReturnInst))
+--   given type constructor and class.
+newtype InstanceDict = InstanceDict (M.Map (TyCon, Class) ClsInst)
 
-instance Monoid SupermonadDict where
-  mappend (SDict a) (SDict b) = SDict $ mappend a b
-  mempty = emptyDict
+instance Monoid InstanceDict where
+  mappend (InstanceDict a) (InstanceDict b) = InstanceDict $ mappend a b
+  mempty = emptyInstDict
 
--- | Class instances of 'Control.Supermonad.Bind'.
-type BindInst        = ClsInst
+-- | The empty instance dictionary.
+emptyInstDict :: InstanceDict
+emptyInstDict = InstanceDict $ M.empty
 
--- | Class instance of 'Control.Supermonad.Applicative'.
-type ApplicativeInst = ClsInst
+-- | Insert an entry into a instance dictionary.
+insertInstDict :: TyCon -> Class -> ClsInst -> InstanceDict -> InstanceDict
+insertInstDict tc cls inst (InstanceDict instDict) 
+  = InstanceDict $ M.insert (tc, cls) inst instDict
 
--- | Class instance of 'Control.Supermonad.Return'.
-type ReturnInst      = ClsInst
-
--- | The empty supermonad dictionary.
-emptyDict :: SupermonadDict
-emptyDict = SDict $ M.empty
-
--- | Insert an entry into a supermonad dictionary.
-insertDict :: TyCon -> Maybe BindInst -> ApplicativeInst -> ReturnInst 
-           -> SupermonadDict -> SupermonadDict
-insertDict tc mClsBind clsApp clsRet (SDict smDict) 
-  = SDict $ M.insert tc (mClsBind, clsApp, clsRet) smDict
-
--- | Try to lookup an entry in a supermonad dictionary.
-lookupDict :: TyCon -> SupermonadDict -> Maybe (Maybe BindInst, ApplicativeInst, ReturnInst)
-lookupDict tc (SDict smDict) = M.lookup tc smDict
-
--- | Try to lookup the 'Control.Supermonad.Bind' instance of the type constructor.
-lookupDictBind :: TyCon -> SupermonadDict -> Maybe BindInst
-lookupDictBind tc smDict = join $ fmap (\(b, _, _) -> b) $ lookupDict tc smDict
-
--- | Try to lookup the 'Control.Supermonad.Applicative' instance of the type constructor.
-lookupDictApplicative :: TyCon -> SupermonadDict -> Maybe ApplicativeInst
-lookupDictApplicative tc smDict = fmap (\(_, a, _) -> a) $ lookupDict tc smDict
-
--- | Try to lookup the 'Control.Supermonad.Return' instance of the type constructor.
-lookupDictReturn :: TyCon -> SupermonadDict -> Maybe ReturnInst
-lookupDictReturn tc smDict = fmap (\(_, _, r) -> r) $ lookupDict tc smDict
+-- | Try to lookup an entry in a instance dictionary.
+lookupInstDict :: TyCon -> Class -> InstanceDict -> Maybe ClsInst
+lookupInstDict tc cls (InstanceDict instDict) = M.lookup (tc, cls) instDict
 
 -- | Retrieve the 'S.Set' of all type constructors in that have an entry in
 --   the supermonad dictionary.
-allTyConsDict :: SupermonadDict -> S.Set TyCon
-allTyConsDict (SDict smDict) = M.keysSet smDict
+allTyConsInstDict :: InstanceDict -> S.Set TyCon
+allTyConsInstDict (InstanceDict instDict) = S.map fst $ M.keysSet instDict
+
+
+
+
