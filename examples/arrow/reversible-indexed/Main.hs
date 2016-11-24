7d344bd0e25f57b5d9663b3d76378b11be06052e
Jan Bracker <jan.bracker@googlemail.com>
2016-11-24 16:22:46 +0000

    Update reversible arrow example.

--- examples/arrow/reversible-indexed/Main.hs
+++ examples/arrow/reversible-indexed/Main.hs
@@ -15,7 +15,7 @@
 
 module Main 
   ( Rev, Iso(..)
-  , rarr, run
+  , rarr, runRev
   , main
   ) where
 
@@ -36,8 +36,8 @@ isoProd :: Iso a b -> Iso c d -> Iso (a,c) (b,d)
 isoProd (Iso f f') (Iso g g') = Iso (\(a,c) -> (f a, g c)) (\(b,d) -> (f' b, g' d))
 
 data Rev (rev :: Bool) (a :: *) (b :: *) where
-  Rev :: Iso a b -> Rev 'True a b
-  NonRev :: Rev 'False a b
+  Rev    :: Iso a b  -> Rev 'True  a b
+  NonRev :: (a -> b) -> Rev 'False a b
 
 type family Or (a :: Bool) (b :: Bool) :: Bool where
   Or 'True  'True  = 'True
@@ -54,36 +54,41 @@ type family And (a :: Bool) (b :: Bool) :: Bool where
 rarr :: (a -> b) -> (b -> a) -> Rev 'True a b
 rarr f g = Rev (Iso f g)
 
-run :: Rev 'True a b -> Iso a b
-run (Rev f) = f
+runRev :: Rev rev a b -> Either (a -> b) (Iso a b)
+runRev (Rev    f) = Right f
+runRev (NonRev f) = Left f
 
 instance Category (Rev 'True) where
   id = Rev id
   (Rev f) . (Rev g) = Rev (f . g)
 
+instance Category (Rev 'False) where
+  id = NonRev id
+  (NonRev f) . (NonRev g) = NonRev (f . g)
+
 instance ArrowArr (Rev 'False) where
-  arr _f = NonRev
+  arr f = NonRev f
 
 instance (t ~ And r s) => ArrowSequence (Rev r) (Rev s) (Rev t)  where
-  (Rev f) >>> (Rev g) = Rev (g . f)
-  (Rev _) >>> NonRev  = NonRev
-  NonRev  >>> (Rev _) = NonRev
-  NonRev  >>> NonRev  = NonRev
+  (Rev f)         >>> (Rev g)         = Rev (g . f)
+  (Rev (Iso f _)) >>> (NonRev g)      = NonRev (g . f)
+  (NonRev f)      >>> (Rev (Iso g _)) = NonRev (g . f)
+  (NonRev f)      >>> (NonRev g)      = NonRev (g . f)
 
 instance (r ~ s) => ArrowSelect (Rev r) (Rev s) where
-  first  (Rev f) = Rev $ isoProd f id 
-  first  NonRev  = NonRev
-  second (Rev f) = Rev $ isoProd id f
-  second NonRev  = NonRev
+  first  (Rev    f) = Rev    $ isoProd f id 
+  first  (NonRev f) = NonRev $ first f
+  second (Rev    f) = Rev    $ isoProd id f
+  second (NonRev f) = NonRev $ second f
 
 instance (r ~ t, s ~ t) => ArrowParallel (Rev r) (Rev s) (Rev t) where
-  (Rev f) *** (Rev g) = Rev $ isoProd f g
-  NonRev  *** NonRev  = NonRev
+  (Rev    f) *** (Rev    g) = Rev $ isoProd f g
+  (NonRev f) *** (NonRev g) = NonRev $ f *** g
 
 instance (r ~ t, s ~ t) => ArrowFanOut (Rev r) (Rev s) (Rev t) where
   (Rev (Iso f f')) &&& (Rev (Iso g _g')) = Rev $ Iso (\a -> (f a, g a)) (\(b, _c) -> f' b)
-  -- If we are talking about actual isomorphism ignoring 'c' should not be a problem.
-  NonRev &&& NonRev = NonRev
+  -- If we are talking about actual isomorphism ignoring c and g' should not be a problem.
+  (NonRev f) &&& (NonRev g) = NonRev $ f &&& g
 
 
 
