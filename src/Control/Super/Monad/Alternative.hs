e38a0028ddaedd402e21f35bae44bc9bb5a0014a
Jan Bracker <jan.bracker@googlemail.com>
2016-12-01 16:05:50 +0000

    src/
    Some preliminary instances for AlternativeEmpty

--- src/Control/Super/Monad/Alternative.hs
+++ src/Control/Super/Monad/Alternative.hs
@@ -9,8 +9,18 @@ module Control.Super.Monad.Alternative
   ) where
 
 import qualified Prelude as P
+import qualified Control.Applicative as A
 
 import GHC.Exts ( Constraint )
+
+import qualified GHC.Generics as Generics
+import qualified GHC.Conc as STM
+import qualified Control.Arrow as Arrow
+import qualified Control.Applicative as Applic
+import qualified Data.Semigroup as Semigroup
+import qualified Data.Proxy as Proxy
+import qualified Text.ParserCombinators.ReadP as ReadP
+import qualified Text.ParserCombinators.ReadPrec as ReadPrec
 
 import Control.Super.Monad.Prelude ( Return(..), Applicative(..) )
 
@@ -19,6 +29,33 @@ class Return f => AlternativeEmpty f where
   type AlternativeEmptyCts f :: Constraint
   type AlternativeEmptyCts f = ()
   empty :: AlternativeEmptyCts f => f a
+
+instance AlternativeEmpty [] where
+  empty = A.empty
+instance AlternativeEmpty P.Maybe where
+  empty = A.empty
+instance AlternativeEmpty P.IO where
+  empty = A.empty
+instance AlternativeEmpty ReadP.ReadP where
+  empty = A.empty
+instance AlternativeEmpty ReadPrec.ReadPrec where
+  empty = A.empty
+instance AlternativeEmpty STM.STM where
+  empty = A.empty
+instance AlternativeEmpty Semigroup.Option where
+  empty = A.empty
+instance AlternativeEmpty Proxy.Proxy where
+  empty = A.empty
+
+-- TODO: ArrowMonad and WrappedMonad instances. These lead to cyclic dependencies.
+
+instance AlternativeEmpty Generics.U1 where
+  empty = A.empty
+instance AlternativeEmpty f => AlternativeEmpty (Generics.Rec1 f) where
+  type AlternativeEmptyCts (Generics.Rec1 f) = AlternativeEmptyCts f
+  empty = Generics.Rec1 empty
+  
+
 
 class Applicative f g h => AlternativeAlt f g h where
   type AlternativeAltCts f g h :: Constraint
