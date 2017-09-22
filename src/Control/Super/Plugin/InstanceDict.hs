9b5dea3a8c7484f4009f56a5db2f52d3ead09e50
Jan Bracker <jan.bracker@googlemail.com>
2017-09-22 15:33:41 +0100

    Reimplments internals of the InstanceDict so that it uses the internal sets and maps

--- src/Control/Super/Plugin/InstanceDict.hs
+++ src/Control/Super/Plugin/InstanceDict.hs
@@ -6,20 +6,21 @@ module Control.Super.Plugin.InstanceDict
   , lookupInstDict, lookupInstDictByTyCon
   , allInstDictTyCons ) where
 
-import Data.Maybe ( fromJust )
-import qualified Data.Set as S
-import qualified Data.Map.Strict as M
+import Data.Maybe ( maybe )
 
 import Class ( Class )
 import InstEnv ( ClsInst )
 import TyCon ( TyCon )
 import qualified Outputable as O
 
+import qualified Control.Super.Plugin.Collection.Set as S
+import qualified Control.Super.Plugin.Collection.Map as M
+
 -- | A dictionary associating supermonad type constructors and classes with 
 --   their instances.
 --   This is essentially a lookup table for instances associated with a 
 --   given type constructor and class.
-newtype InstanceDict = InstanceDict (M.Map (TyCon, Class) ClsInst)
+newtype InstanceDict = InstanceDict (M.Map TyCon (M.Map Class ClsInst))
 
 instance Monoid InstanceDict where
   mappend (InstanceDict a) (InstanceDict b) = InstanceDict $ mappend a b
@@ -35,26 +36,23 @@ emptyInstDict = InstanceDict $ M.empty
 -- | Insert an entry into a instance dictionary.
 insertInstDict :: TyCon -> Class -> ClsInst -> InstanceDict -> InstanceDict
 insertInstDict tc cls inst (InstanceDict instDict) 
-  = InstanceDict $ M.insert (tc, cls) inst instDict
+  = InstanceDict $ M.insert tc (M.insert cls inst (maybe M.empty id $ M.lookup tc instDict)) instDict
 
 -- | Try to lookup an entry in a instance dictionary.
 lookupInstDict :: TyCon -> Class -> InstanceDict -> Maybe ClsInst
-lookupInstDict tc cls (InstanceDict instDict) = M.lookup (tc, cls) instDict
+lookupInstDict tc cls (InstanceDict instDict) = do
+  clsDict <- M.lookup tc instDict
+  M.lookup cls clsDict
 
 -- | Retrieve the 'S.Set' of all type constructors in that have an entry in
 --   the supermonad dictionary.
 allInstDictTyCons :: InstanceDict -> S.Set TyCon
-allInstDictTyCons (InstanceDict instDict) = S.map fst $ M.keysSet instDict
+allInstDictTyCons (InstanceDict instDict) = M.keysSet instDict
 
 -- | Looks up all entries with the given type constructor as key.
 --   Returns a mapping between the classes and their instances for that
 --   type constructor.
 lookupInstDictByTyCon :: TyCon -> InstanceDict -> M.Map Class ClsInst
-lookupInstDictByTyCon tc (InstanceDict instDict) 
-  -- The use of fromJust is safe here, because the keys we look up were
-  -- taken from the existing entries of the map we are looking into.
-  = foldr (\cls m -> M.insert cls (fromJust $ M.lookup (tc, cls) instDict) m) M.empty clsKeys
-  where
-    clsKeys = fmap snd $ filter ((tc ==) . fst) $ M.keys instDict
+lookupInstDictByTyCon tc (InstanceDict instDict) = maybe M.empty id $ M.lookup tc instDict
 
 
