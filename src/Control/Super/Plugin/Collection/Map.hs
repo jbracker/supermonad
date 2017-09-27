00d732f49077a327f65d42bcb7bce9ab6daacb22
Jan Bracker <jan.bracker@googlemail.com>
2017-09-27 14:57:12 +0100

    Documentation of the internal repesentation of Maps and Sets.

--- src/Control/Super/Plugin/Collection/Map.hs
+++ src/Control/Super/Plugin/Collection/Map.hs
@@ -3,7 +3,8 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
--- | Provides the type for instance dictionaries.
+-- | Provides a internal implementation of 'Data.Map.Map's that can be used 
+--   with GHC types that are 'Uniquable' but not 'Ord'erable.
 module Control.Super.Plugin.Collection.Map 
   ( Map
   , empty
@@ -28,12 +29,15 @@ import qualified Outputable as O
 
 import qualified Control.Super.Plugin.Collection.Set as S
 
+-- | A map with keys of type @k@ and elements of type @a@.
 newtype Map k a = Map { unMap :: UniqFM (k, a) } deriving Data
 
+-- | Maps can be checked for equality if their elements and keys allow it.
 instance (Eq k, Eq a) => Eq (Map k a) where
   ma == mb = unMap ma == unMap mb
   ma /= mb = unMap ma /= unMap mb
 
+-- | Monoid based on union and the empty map.
 instance Monoid (Map k a) where
   mempty = empty
   mappend = union
@@ -42,21 +46,28 @@ instance Monoid (Map k a) where
 instance (O.Outputable a, O.Outputable k) => O.Outputable (Map k a) where
   ppr = (O.ppr) . unMap
 
+-- | The empty map.
 empty :: Map k a
 empty = Map $ U.emptyUFM
 
+-- | Is the map empty?
 null :: Map k a -> Bool
 null ma = U.isNullUFM $ unMap ma
 
+-- | Count the number of entries in the map.
 size :: Map k a -> Int
 size ma = U.sizeUFM $ unMap ma
 
+-- | Check if the given key has an entry in the map.
 member :: Uniquable k => k -> Map k a -> Bool
 member k ma = U.elemUFM k $ unMap ma
 
+-- | Check if the given key does not have an entry in the map.
 notMember :: Uniquable k => k -> Map k a -> Bool
 notMember k ma = not $ member k ma
 
+-- | Insert the given key value pair in the map. Any preexisting 
+--   entry with the same key will be replaced.
 insert :: forall k a. Uniquable k => k -> a -> Map k a -> Map k a
 insert k e m = Map $ U.alterUFM (Just . f) (unMap m) k
   where f :: Maybe (k , a) -> (k , a)
@@ -67,35 +78,54 @@ insert k e m = Map $ U.alterUFM (Just . f) (unMap m) k
         -- Ignore non matching keys
         f (Just entry) = entry
 
+-- | Retrieve the associated entry of the given key, if there is one.
 lookup :: Uniquable k => k -> Map k a -> Maybe a
 lookup k m = fmap snd $ U.lookupUFM (unMap m) k
 
+-- | Remove the entry with the given key, if it exists.
 delete :: Uniquable k => k -> Map k a -> Map k a
 delete k m = Map $ U.delFromUFM (unMap m) k
 
+-- | Merge together two maps. If there are two entries with the 
+--   same key, the left (first) map will be prefered (left bias).
 union :: Map k a -> Map k a -> Map k a
 union ma mb = Map $ U.plusUFM_C (\a _ -> a) (unMap ma) (unMap mb)
 
+-- | Merge several maps together. If there are two entries with the 
+--   same key, the left-most map in the list will be prefered (left bias).
 unions :: [Map k a] -> Map k a
 unions ms = foldl union empty ms
 
+-- | Maps the entries of a map using the given function.
 map :: (a -> b) -> Map k a -> Map k b
 map f ma = Map $ U.mapUFM (\(k,e) -> (k,f e)) $ unMap ma
 
+-- | Filter the value of a map using the given predicate.
+--   Only thoes entries that the predicate yields 'True' for
+--   will be kept.
 filter :: (a -> Bool) -> Map k a -> Map k a
 filter p ma = Map $ U.filterUFM (p . snd) $ unMap ma
 
+-- | Convert the map into a list of key value pairs.
 toList :: Map k a -> [(k, a)]
 toList m = U.eltsUFM $ unMap m
 
+-- | Create a map from a list of key value pairs.
+--   If there are several pairs with the same key the entry
+--   of the last pair in the list with that key will be kept.
 fromList :: Uniquable k => [(k, a)] -> Map k a
 fromList l = Map $ U.listToUFM $ fmap (\(k, a) -> (k , (k , a))) l
 
+-- | Create a list of all entries in the map. There is no
+--   guarenteed order for the list.
 elems :: Map k a -> [a]
 elems ma = fmap snd $ toList ma
 
+-- | Create a 'Set' containing all the keys in the map.
 keysSet :: Uniquable k => Map k a -> S.Set k
 keysSet ma = S.fromList $ keys ma
 
+-- | Create a list containing all the keys in the map.
+--   There is no guarenteed order for the list.
 keys :: Map k a -> [k]
 keys ma = fmap fst $ toList ma
--- src/Control/Super/Plugin/Collection/Set.hs
+++ src/Control/Super/Plugin/Collection/Set.hs
@@ -2,7 +2,8 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE DeriveDataTypeable #-}
 
--- | Provides the type for instance dictionaries.
+-- | Provides a internal implementation of 'Data.Set.Set's that can be used 
+--   with GHC types that are 'Uniquable' but not 'Ord'erable.
 module Control.Super.Plugin.Collection.Set 
   ( Set
   , empty, singleton
@@ -24,12 +25,15 @@ import qualified UniqSet as U
 import qualified Outputable as O 
 import Control.Super.Plugin.Wrapper ( uniqSetToList )
 
+-- | A set with elements of type @a@.
 newtype Set a = Set { unSet :: UniqSet a } deriving Data
 
+-- | Sets can be checked for equality if their elements allow it.
 instance (Eq a) => Eq (Set a) where
   sa == sb = unSet sa == unSet sb
   sa /= sb = unSet sa /= unSet sb
 
+-- | Monoid based on union and the empty set.
 instance Monoid (Set a) where
   mempty = empty
   mappend = union
@@ -38,61 +42,90 @@ instance Monoid (Set a) where
 instance (O.Outputable a) => O.Outputable (Set a) where
   ppr = (O.ppr) . unSet 
 
+-- | The empty set.
 empty :: Set a
 empty = Set $ U.emptyUniqSet
 
+-- | Create a set with exactly one element.
 singleton :: Uniquable a => a -> Set a
 singleton a = Set $ U.unitUniqSet a
 
+-- | Is the set empty?
 null :: Set a -> Bool
 null s = U.isEmptyUniqSet $ unSet s
 
+-- | Compute the number of elements in the set.
 size :: Set a -> Int
 size s = U.sizeUniqSet $ unSet s
 
+-- | Check if a given element is in a set.
 member :: Uniquable a => a -> Set a -> Bool
 member a s = U.elementOfUniqSet a $ unSet s
 
+-- | Check if a given element is not in a set.
 notMember :: Uniquable a => a -> Set a -> Bool
 notMember a s = not $ member a s
 
+-- | Check if the first set is a subset of the second.
+--   The sets may be equal.
 isSubsetOf :: Ord a => Set a -> Set a -> Bool
 isSubsetOf sub super = intersection sub super == sub
 
+-- | Check if the first set is a subset of the second.
+--   The sets may not be equal.
 isProperSubsetOf :: Ord a => Set a -> Set a -> Bool
 isProperSubsetOf sub super = let i = intersection sub super in i == sub && i /= super
 
+-- | Insert a given element into a set. This will replace any
+--   preexisting element with the same unique reprentation in 
+--   the set.
 insert :: Uniquable a => a -> Set a -> Set a
 insert a s = Set $ U.addOneToUniqSet (unSet s) a
 
+-- | Delete a given element from a set. This will remove 
+--   any preexisting element with the same unique reprentation in 
+--   the set.
 delete :: Uniquable a => a -> Set a -> Set a
 delete a s = Set $ U.delOneFromUniqSet (unSet s) a
 
+-- | Map the element of a set using the given function.
 map :: Uniquable b => (a -> b) -> Set a -> Set b
 -- We need to use a custom implementation of "U.mapUniqSet",
 -- because back in GHC 7.10 it was implemented wrong.
 map f s = Set $ (U.mkUniqSet . fmap f . uniqSetToList) $ unSet s
 
+-- | Filter the elements of a set using the given predicate.
+--   Only those elements where the predicate yields 'True' are kept.
 filter :: (a -> Bool) -> Set a -> Set a
 filter p s = Set $ U.filterUniqSet p $ unSet s
 
+-- | Merge two sets into one with a union.
 union :: Set a -> Set a -> Set a
 union sa sb = Set $ U.unionUniqSets (unSet sa) (unSet sb)
 
+-- | Merge a list of sets into one using a union.
 unions :: [Set a] -> Set a
 unions ss = Set $ U.unionManyUniqSets $ fmap unSet ss
 
+-- | Merge two set, but only keep those elements that 
+--   were present in both sets.
 intersection :: Set a -> Set a -> Set a
 intersection sa sb = Set $ U.intersectUniqSets (unSet sa) (unSet sb)
 
+-- | Remove all element that are in the second set from the first one.
 difference :: Set a -> Set a -> Set a
 difference sa sb = Set $ U.minusUniqSet (unSet sa) (unSet sb)
 
+-- | Remove all element that are in the second set from the first one.
 (\\) :: Set a -> Set a -> Set a
 (\\) = difference
 
+-- | Convert the set into a list. There is not guarenteed order for the
+--   elements to appear in.
 toList :: Set a -> [a]
 toList s = uniqSetToList $ unSet s
 
+-- | Convert a list into a set. If there are several elements with the 
+--   same unique representation only one of them will be kept.
 fromList :: Uniquable a => [a] -> Set a
 fromList l = Set $ U.mkUniqSet l
