759205dcba7f3901b4e800ea36c4ccbee6e079d4
Jan Bracker <jan.bracker@googlemail.com>
2017-11-21 13:38:41 +0000

    Update the missing function example

--- examples/test/missing-functions/Main.hs
+++ examples/test/missing-functions/Main.hs
@@ -116,6 +116,19 @@ import Control.Super.Monad.Constrained.Functions
   , ap
   , (<$!>), (<$>)
   , ifThenElse
+  
+  , liftA3, liftA2, liftA
+  , voidA
+  , (<**>)
+  , mapA, mapA_
+  , forA, forA_
+  , filterA
+  , sequenceA, sequenceA_
+  , traverse
+  , zipWithA, zipWithA_
+  , mapAndUnzipA
+  , replicateA, replicateA_
+  , whenA, unlessA
   )
 
 main :: P.IO ()
